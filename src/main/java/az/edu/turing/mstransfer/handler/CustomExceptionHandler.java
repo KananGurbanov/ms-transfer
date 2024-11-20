d4617e252a1c872c9a0b7b5dd868fd241ca03851
Kanan Gurbanov <knanqurbanov22@gmail.com>
2024-11-20 10:22:03 +0400

    code refactored

--- src/main/java/az/edu/turing/mstransfer/handler/CustomExceptionHandler.java
+++ src/main/java/az/edu/turing/mstransfer/handler/CustomExceptionHandler.java
@@ -7,7 +7,6 @@ import az.edu.turing.mstransfer.model.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-//import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.ServletWebRequest;
